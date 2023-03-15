c78d54c3daf2086b938c4b0582a07e055a3335f9
Administrator <Administrator@172.20.0.150>
2023-03-15 14:04:50 +0530

    Updated Code day 3

--- src/test/java/com/citi/base/AutomationWrapper.java
+++ src/test/java/com/citi/base/AutomationWrapper.java
@@ -3,11 +3,31 @@ package com.citi.base;
 import org.openqa.selenium.WebDriver;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 
 public class AutomationWrapper {
 	
 	public WebDriver driver;
 	
+	@BeforeStep
+	public void beforeStep()
+	{
+		
+	}
+	
+	@AfterStep
+	public void afterStep()
+	{
+		
+	}
+	
+	@Before
+	public void startScenario()
+	{
+		
+	}
 	
 	@After
 	public void endScenario()
